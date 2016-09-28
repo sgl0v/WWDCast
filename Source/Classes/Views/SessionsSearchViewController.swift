5183a9c31929063370e7235dc9253fe4ff28faf5
Maksym Shcheglov <maxscheglov@gmail.com>
2016-09-28 17:55:37 +0200

    Reverted the latest SessionsSearchViewController changes.

--- Source/Classes/Views/SessionsSearchViewController.swift
+++ Source/Classes/Views/SessionsSearchViewController.swift
@@ -11,16 +11,14 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 
-class SessionsSearchViewController: UIViewController {
+class SessionsSearchViewController: TableViewController<SessionSectionViewModel, SessionTableViewCell> {
     
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var tableView: UITableView!
     private let viewModel: SessionsSearchViewModel
-    private let disposeBag = DisposeBag()
     
     init(viewModel: SessionsSearchViewModel) {
         self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
+        super.init()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -38,26 +36,12 @@ class SessionsSearchViewController: UIViewController {
     private lazy var searchController: UISearchController = {
         let searchController = UISearchController(searchResultsController: nil)
         searchController.dimsBackgroundDuringPresentation = false
-//        searchController.hidesNavigationBarDuringPresentation = false
         return searchController
     }()
     
     private var searchBar: UISearchBar {
         return self.searchController.searchBar
     }
-    
-    private lazy var source: RxTableViewSectionedReloadDataSource<SessionSectionViewModel> = {
-        let dataSource = RxTableViewSectionedReloadDataSource<SessionSectionViewModel>()
-        dataSource.configureCell = { (dataSource, tableView, indexPath, element) in
-            let cell = tableView.dequeueReusableCell(withClass: SessionTableViewCell.self, forIndexPath: indexPath)
-            cell.bindViewModel(element)
-            return cell
-        }
-        dataSource.titleForHeaderInSection = { (dataSource, section) in
-            return dataSource.sectionAtIndex(section).description
-        }
-        return dataSource
-    }()
     
     private func bindViewModel() {
         // ViewModel's input
@@ -66,25 +50,21 @@ class SessionsSearchViewController: UIViewController {
         self.tableView.rx_modelSelected(SessionItemViewModel.self)
             .bindNext(self.viewModel.itemSelectionObserver)
             .addDisposableTo(self.disposeBag)
-        self.tableView.rx_itemSelected.subscribeNext({ indexPath in
-            self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        }).addDisposableTo(self.disposeBag)
         
         // ViewModel's output
         
         self.viewModel.sessionSections.drive(self.tableView.rx_itemsWithDataSource(self.source)).addDisposableTo(self.disposeBag)
         self.viewModel.title.drive(self.rx_title).addDisposableTo(self.disposeBag)
-        self.viewModel.isLoading.drive(self.tableView.rx_hidden).addDisposableTo(self.disposeBag)
-        self.viewModel.isLoading.drive(self.loadingIndicator.rx_animating).addDisposableTo(self.disposeBag)
+        self.viewModel.isLoading.drive(self.view.rx_hidden).addDisposableTo(self.disposeBag)
+//        self.viewModel.isLoading.drive(self.loadingIndicator.rx_animating).addDisposableTo(self.disposeBag)
     }
 
     private func configureUI() {
-        self.edgesForExtendedLayout = .None
         self.definesPresentationContext = true
         self.navigationItem.rightBarButtonItem = UIBarButtonItem.castBarButtonItem()
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: NSLocalizedString("Filter", comment: "Filter"), style: .Plain, target: nil, action: nil)
 
-//        self.clearsSelectionOnViewWillAppear = true
+        self.clearsSelectionOnViewWillAppear = true
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 100
         self.tableView.delegate = nil
