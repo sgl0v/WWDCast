85e00f6a0e6ce06ebb6db5e15712ff9a0fabe567
Maksym Shcheglov <maxscheglov@gmail.com>
2016-10-20 11:23:12 +0200

    Moved TabBarController to the dedicated file.

--- Source/Classes/WWDCastAssemblyImpl.swift
+++ Source/Classes/WWDCastAssemblyImpl.swift
@@ -7,37 +7,6 @@
 //
 
 import UIKit
-import GoogleCast
-
-class TabBarController: UITabBarController, GCKUIMiniMediaControlsViewControllerDelegate {
-    
-    private var miniMediaControlsViewController: GCKUIMiniMediaControlsViewController!
-    private static let kMiniMediaControlsViewHeight: CGFloat = 60.0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let castContext = GCKCastContext.sharedInstance()
-        castContext.useDefaultExpandedMediaControls = true
-        
-        miniMediaControlsViewController = castContext.createMiniMediaControlsViewController()
-        miniMediaControlsViewController.delegate = self
-        self.addChildViewController(miniMediaControlsViewController)
-        self.view.addSubview(miniMediaControlsViewController.view)
-        miniMediaControlsViewController.didMoveToParentViewController(self)
-        miniMediaControlsViewController.view.hidden = !miniMediaControlsViewController.active
-    }
-    
-    func miniMediaControlsViewController(miniMediaControlsViewController: GCKUIMiniMediaControlsViewController, shouldAppear: Bool) {
-        miniMediaControlsViewController.view.hidden = !miniMediaControlsViewController.active
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        let y = CGRectGetMaxY(self.view.bounds) - TabBarController.kMiniMediaControlsViewHeight - CGRectGetHeight(self.tabBar.frame)
-        miniMediaControlsViewController.view.frame = CGRectMake(0, y, CGRectGetWidth(self.view.bounds), TabBarController.kMiniMediaControlsViewHeight)
-    }
-}
 
 @objc class WWDCastAssemblyImpl: NSObject, WWDCastAssembly {
     
