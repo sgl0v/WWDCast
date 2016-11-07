960208db9ce882114af4b266dcf752714cf52543
Maksym Shcheglov <maxscheglov@gmail.com>
2016-11-07 22:50:07 +0100

    Added UIActivityIndicatorView to the search session view. Moved TabBarController to the views folder. The UISearchController+UITableView doesn't work as expected with UI defined in the xib file.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+
 # WWDCast
+
 iOS application to cast WWDC sessions
+
+####TODO
+* show `No favorites` message instead of the blank view, when the user doesn't have favorite sessions;
+* split the sessions cache into two: sessions and favorites;
+* adjust the tableview when the ChromeCast media control is visible;
+* show ChromeCast media controls when the session starts playing;
+* handle the volume level changes;
+* show the session info @ iPhone control center.
+
--- Source/Classes/Views/SessionsSearchViewController.swift
+++ Source/Classes/Views/SessionsSearchViewController.swift
@@ -11,14 +11,31 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 
-class SessionsSearchViewController: TableViewController<SessionSectionViewModel, SessionTableViewCell> {
+class SessionsSearchViewController: UIViewController
+{
     
-    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
-    fileprivate let viewModel: SessionsSearchViewModel
+    private var loadingIndicator: UIActivityIndicatorView!
+    private var tableView: UITableView!
+    private let viewModel: SessionsSearchViewModel
+    private let disposeBag = DisposeBag()
+    
+    lazy var source: RxTableViewSectionedReloadDataSource<SessionSectionViewModel> = {
+        let dataSource = RxTableViewSectionedReloadDataSource<SessionSectionViewModel>()
+        dataSource.configureCell = { (dataSource, tableView, indexPath, element) in
+            let cell = tableView.dequeueReusableCell(withClass: SessionTableViewCell.self, forIndexPath: indexPath)!
+            cell.bindViewModel(element)
+            return cell
+        }
+        dataSource.titleForHeaderInSection = { (dataSource: TableViewSectionedDataSource<SessionSectionViewModel>, sectionIndex: Int) -> String? in
+            return dataSource[sectionIndex].description
+        }
+        return dataSource
+    }()
+
     
     init(viewModel: SessionsSearchViewModel) {
         self.viewModel = viewModel
-        super.init()
+        super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -33,17 +50,18 @@ class SessionsSearchViewController: TableViewController<SessionSectionViewModel,
     
     // MARK - Private
     
-    fileprivate lazy var searchController: UISearchController = {
+    private lazy var searchController: UISearchController = {
         let searchController = UISearchController(searchResultsController: nil)
         searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.tintColor = UIColor.black
         return searchController
     }()
     
-    fileprivate var searchBar: UISearchBar {
+    private var searchBar: UISearchBar {
         return self.searchController.searchBar
     }
     
-    fileprivate func bindViewModel() {
+    private func bindViewModel() {
         // ViewModel's input
         self.navigationItem.leftBarButtonItem!.rx.tap.bindNext(self.viewModel.filterObserver).addDisposableTo(self.disposeBag)
         self.searchQuery.drive(onNext: self.viewModel.searchStringObserver).addDisposableTo(self.disposeBag)
@@ -55,16 +73,22 @@ class SessionsSearchViewController: TableViewController<SessionSectionViewModel,
         
         self.viewModel.sessionSections.drive(self.tableView.rx.items(dataSource: self.source)).addDisposableTo(self.disposeBag)
         self.viewModel.title.drive(self.rx.title).addDisposableTo(self.disposeBag)
-        self.viewModel.isLoading.drive(self.view.rx.isHidden).addDisposableTo(self.disposeBag)
-//        self.viewModel.isLoading.drive(self.loadingIndicator.rx.animating).addDisposableTo(self.disposeBag)
+        self.viewModel.isLoading.drive(self.tableView.rx.isHidden).addDisposableTo(self.disposeBag)
+        self.viewModel.isLoading.drive(self.loadingIndicator.rx.isAnimating).addDisposableTo(self.disposeBag)
     }
 
-    fileprivate func configureUI() {
+    private func configureUI() {
         self.definesPresentationContext = true
         self.navigationItem.rightBarButtonItem = UIBarButtonItem.castBarButtonItem()
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: NSLocalizedString("Filter", comment: "Filter"), style: .plain, target: nil, action: nil)
 
-        self.clearsSelectionOnViewWillAppear = true
+//        self.clearsSelectionOnViewWillAppear = true
+        
+        self.view.backgroundColor = UIColor.white
+        
+        self.tableView = UITableView(frame:self.view.frame)
+        self.view.addSubview(self.tableView)
+        self.tableView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 100
         self.tableView.delegate = nil
@@ -73,19 +97,22 @@ class SessionsSearchViewController: TableViewController<SessionSectionViewModel,
         self.tableView.layoutMargins = UIEdgeInsets.zero
         self.tableView.tableFooterView = UIView()
         self.tableView.registerNib(cellClass: SessionTableViewCell.self)
-        
         // dismiss keyboard on scroll
-        self.tableView.rx.contentOffset.asDriver()
-            .filter({[unowned self] _ -> Bool in
-                return !self.searchController.isBeingPresented
-                }).drive(onNext: {[unowned self] _ in
-                if self.searchBar.isFirstResponder {
-                    self.searchBar.resignFirstResponder()
-                }
-            }).addDisposableTo(self.disposeBag)
+        self.tableView.rx.contentOffset.asDriver().filter({[unowned self] _ -> Bool in
+            return !self.searchController.isBeingPresented && self.searchBar.isFirstResponder
+        }).drive(onNext: {[unowned self] _ in
+            self.searchBar.resignFirstResponder()
+        }).addDisposableTo(self.disposeBag)
+        
+        self.loadingIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        self.view.addSubview(self.loadingIndicator)
+        self.loadingIndicator.hidesWhenStopped = true
+        self.loadingIndicator.frame.origin.x = self.view.bounds.midX
+        self.loadingIndicator.frame.origin.y = self.view.bounds.midY
+        self.loadingIndicator.autoresizingMask = [.flexibleLeftMargin, .flexibleTopMargin, .flexibleRightMargin, .flexibleBottomMargin]
     }
 
-    fileprivate var searchQuery: Driver<String> {
+    private var searchQuery: Driver<String> {
         let cancel: Observable<String> = searchController.searchBar.rx.delegate.methodInvoked(#selector(UISearchBarDelegate.searchBarCancelButtonClicked(_:))).map( { _ in return "" })
         
         let searchBarTextObservable = self.searchBar.rx.text.filter({ $0 != nil }).map({ $0! })
